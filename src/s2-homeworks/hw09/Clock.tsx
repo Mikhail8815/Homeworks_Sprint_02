3c6740ab5c1a446377853c3ba6520f0642ed9225
Mikhail8815 <mishutka150588@mail.ru>
2025-03-26 23:48:00 +0300

    SP_04_week_01(1)

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -15,6 +15,7 @@ function Clock() {
                 setDate(new Date());
             }, 1000);
             setTimerId(id);
+            //
         }
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
